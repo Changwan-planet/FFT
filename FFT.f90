cfc676196536c06d0124a2b4900abce77b389675
Changwan <tjsckddhks@gmail.com>
2020-12-28 22:09:18 +0900

    required to revise the subroutine

--- FFT.f90
+++ FFT.f90
@@ -3,7 +3,7 @@ Implicit none
 
 !INPUT DATA & INITIALIZATION
 INTEGER,PARAMETER :: S=16      !THE NUMBER OF SAMPLE POINT
-INTEGER           :: N        !THE NUMBER OF SAMPLE POINT FOR DO LOOP   
+INTEGER           :: N=S        !THE NUMBER OF SAMPLE POINT FOR DO LOOP   
 REAL*8, PARAMETER :: pi=Acos(-1.0)
 INTEGER           :: K        !SAMPLE POINT
 
@@ -30,7 +30,7 @@ COMPLEX :: W=EXP(-2*pi/S)
 INTEGER :: M 
 INTEGER :: T1                 !TEMPORARY VALUE
 INTEGER :: T3                 !TEMPORARY VALUE
-INTEGER ::  t
+INTEGER :: t
 REAL*8 :: f=1.0d+1
 
 OPEN(10, FILE="sine_testFFT.txt", status='replace')
@@ -38,40 +38,41 @@ OPEN(11, FILE="output_testFFT.txt",status='replace')
 
 !=======INPUTDATA=============
 f=1
-Do t=1,S/2
-   XREAL(t)=cos(2*pi*f*((t-1)/(S-1)))
-   Print *, "====start==="
-   Print *, f
-   Print *, pi
-   Print *, t
-   Print *, S
-   Print *, "test=",2*pi*f*(t-1)/(S-1)
-   Print *, 2*pi*f*((t-1)/(S-1))
-   Print *, "XREAL(",t,")=",XREAL(t)
-   !Print *, pi
-   !Print *, sin(2*pi/4)
-   !WRITE(10,*) XREAL(t)
+
+Do t=1, S
+
+   XREAL(t)=cos(2*pi*f*(t-1)/(S-1))
+!   Print *, "XREAL(",t,")=",XREAL(t)
+   WRITE(10,*) XREAL(t)
+
 END DO 
-   !Print *, XREAL
+
 !=============================
 
 
 !RELATIONSHIPS & INITIALIZATION 
-K=0
-
-!======DEFINE NU (GAMMA)=======
+K = 0
 NU = 0
+!======DEFINE NU (GAMMA)=======
+!NU = 0
     
-DO WHILE(N==1)
+DO 
 
-  N=INT(S/2)
+IF (N==1) EXIT
+!  PRINT*, "N=",N
+!  PRINT*, "N/2=",N/2
+  N=INT(N/2)
   NU=NU+1
+  
+!  PRINT*, "N=",N
+!  PRINT*, "NU(GAMMA)=",NU  
 
-END DO 
+END DO
+ 
 !==============================
 
 
-PRINT*, "NU (GAMMA)=",NU
+PRINT*, "NU(GAMMA)=",NU
 
   DO L=1,NU
 
@@ -80,15 +81,16 @@ PRINT*, "NU (GAMMA)=",NU
     
 110   DO I=1,N2
        
-        PRINT *, "I=",I, "N2=",N2
+!        PRINT *, "I=",I, "N2=",N2
         !J=I/(2**(L))            
          NU1=NU-L
-
+!        PRINT *, "NU1=",NU1 
 
 !100 IF (L < NU)    !TO SEE IF ALL ARRAYS HAVE BEEN COMPUTED
 
-
+                 
 120      M=INT(K/(2**NU1)) 
+         PRINT*, "M=",M
          CALL bit_reversing(m,NU,P)      
          !P=br
          print *, "P=",P
@@ -154,13 +156,16 @@ CONTAINS
             
  SUBROUTINE  bit_reversing(m,NU,br)
    IMPLICIT NONE
-   INTEGER              :: m, NU
+   INTEGER              :: m,NU
    INTEGER              :: j2, i1
    INTEGER, INTENT(OUT) :: br
 
    !INITIALIZATION
      i1 = 1
      br = 0
+     PRINT*, "m=",m
+     PRINT*, "NU=",NU
+     PRINT*, "br1=",br
 
 150  IF (i1>NU) THEN
          RETURN 
@@ -168,13 +173,15 @@ CONTAINS
      ELSE 
          j2 = INT(m/2)
          br = 2*br + (m-2*j2)
+         !PRINT*, "br2=",br
          m  = j2
 
          i1=i1+1
 
          GO TO 150
      END IF 
-
+    
+    PRINT*, "br3=",br   
  END SUBROUTINE bit_reversing
 
 END PROGRAM FFT
